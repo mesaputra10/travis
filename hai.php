199cb222028d56b921203ca648f4e5619c3b42b2
root <adisaputra.id@gmail.com>
2018-12-20 10:08:31 +0000

    sonar ok

--- hai.php
+++ hai.php
@@ -1,16 +1,1 @@
-<?php 
-echo "hia a";
-
-hi;
-
-
-hi;
-asklklsk;
-sss
-
-getData(); 
-getSatu();
-
-test()
-
-?>
+benar
--- index.php
+++ index.php
@@ -1,13 +1,1 @@
-echo "Testing environment ,travis aws key";
-echo $awskey;
-<?php
-
-function test() {
-
-    gts;
-}
-
-
-?>
-
-<?php jajaja ?>
+<?php echo "testt"; ?>
